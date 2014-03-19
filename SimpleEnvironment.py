964680642672e8cb2fd51b3ffd5bd3117703d124
Shawn Hanna <shawnkarlhanna@gmail.com>
2014-03-18 20:34:46 -0400

    Steven's code

--- SimpleEnvironment.py
+++ SimpleEnvironment.py
@@ -58,6 +58,7 @@ class SimpleEnvironment(object):
 
 
     def Extend(self, start_config, end_config):
+
         # TODO: Implement a function which attempts to extend from
         #   a start configuration to a goal configuration
         if self.ComputeDistance(start_config,end_config) < self.epsilon:
@@ -66,7 +67,26 @@ class SimpleEnvironment(object):
             theta = math.atan2(end_config[1]-start_config[1],end_config[0]-start_config[0])
             return start_config[0] + self.epsilon*math.cos(theta), start_config[1] + self.epsilon*math.sin(theta)
 
-        pass
+
+        travel_limit = 0.01 # maximum distance allowed to travel towards goal
+        num_checks = 10 # number of collision checks along the way
+        dist = self.ComputeDistance(start_config, end_config)
+
+        travel_dist = min(travel_limit, dist)
+
+        x0 = start_config[0]
+        y0 = start_config[1]
+        x1 = end_config[0]
+        y1 = end_config[1]
+        xg = x0 + ( (travel_dist/dist)*(x1-x0) ) # xg and yg limited by travel_limit
+        yg = y0 + ( (travel_dist/dist)*(y1-y0) )
+
+        # incrementally step along line from start to goal, checking for collisions
+        for i in range(0, num_checks-1):
+            config[0] = x0 + ( (i/(num_checks-1)) * (xg-x0) )
+            config[1] = y0 + ( (i/(num_checks-1)) * (yg-y0) )
+            if self.Collides(config):
+                return None
 
 
     def ShortenPath(self, path, timeout=5.0):
@@ -79,21 +99,16 @@ class SimpleEnvironment(object):
 
 
     def Collides(self, point, theta):
-        # Show all obstacles in environment
-        for b in self.robot.GetEnv().GetBodies():
-            if b.GetName() == self.robot.GetName():
-                continue
-            bb = b.ComputeAABB()
-            tl = [bb.pos()[0] - bb.extents()[0], bb.pos()[1]-bb.extents()[1]]
-            br = [bb.pos()[0] + bb.extents()[0], bb.pos()[1]+bb.extents()[1]]
+        T = self.robot.GetTransform()
+        Tnew = T
+        Tnew[[0,1],3] = point
+        env = self.robot.GetEnv()
 
-            x = point[0]
-            y = point[1]
+        # move robot to potential point, return collision between robot and table
+        self.robot.SetTransform(Tnew)
+        return env.CheckCollision(self.robot, env.GetKinBody('conference_table'))
 
-            if ((x <= br[0]) and (x >= tl[0]) and (y <= tl[1]) and (y >= br[1])):
-                return True
 
-            return False
 
 
     # Returns true if the point specified collides with an object in the space
